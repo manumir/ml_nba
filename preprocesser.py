d875e4eb98ee2a39de155087a65b647d0f8a6fb0
random <random123@gmail.com>
2019-07-30 10:13:39 +0100

    create model.py

--- preprocesser.py
+++ preprocesser.py
@@ -22,25 +22,6 @@ for ix in range(len(data)):
   
   for c in c2_avg:
     data.at[ix,c]=f.get_avgs(past,c)
-'''
-a=data.loc[[0]]
-x=0
-while ix < len(data):
-  if ix != 0:
-    a=a.append(data.loc[[ix]])
-  ix+=2
 
-b=data.loc[[1]]
-ix=1
-while ix < len(data):
-  if ix != 1:
-    b=b.append(data.loc[[ix]])
-  ix+=2
-
-a=a.reset_index(drop=True)
-b=b.reset_index(drop=True)
-
-b=b.join(a,lsuffix='_left',rsuffix='_right')
-'''
 data.to_csv('train.csv',index=False)
 

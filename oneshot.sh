c4c38eebebf2efe95e40bd72ef82e8a596e1a7cd
manumir <miranda.junior@live.com.pt>
2020-02-28 17:52:42 +0000

    a

--- oneshot.sh
+++ oneshot.sh
@@ -12,8 +12,8 @@ python3 plac_preds.py
 cd models
 python3 linear_regression.py
 python3 mlp.py
-python3 svm.py
-python3 xgb.py
+#python3 svm.py
+#python3 xgb.py
 #py trees.py #too much timee to run
 #py rf.py #too much timee to run
 cd ..

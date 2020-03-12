8471e23d066d65a10fbd710778396fbfcb0410ff
manumir <miranda.junior@live.com.pt>
2020-03-12 11:30:32 +0000

    a

--- logs/bets_simulation.py
+++ logs/bets_simulation.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import datetime
 import os
 
 path2og=os.getcwd()[:-4]
@@ -15,21 +17,52 @@ lin=pd.read_csv('linear_log.csv')
 data2calc=pd.DataFrame()
 for date in list(set(list(lin['date'].values))):
 	data2calc=data2calc.append(data.loc[data['Game Date_home']==date])
+data2calc['Game Date_home']=data2calc['Game Date_home'].astype('datetime64[ns]')
 data2calc=data2calc.sort_values(['Game Date_home','Team_home'])
+new=[]
+for x in data2calc['Game Date_home']:
+	x=str(x)
+	x=x.replace('-','/')
+	x=x[:10]
+	x=datetime.datetime.strptime(x,"%Y/%m/%d")
+	x=x.strftime("%m/%d/%Y")
+	new.append(x)
+data2calc['Game Date_home']=new
 data2calc.reset_index(inplace=True,drop=True)
 
+# get plac data on lin predicted games and sort by date
 plac_lin=pd.DataFrame()
-for date in list(set(list(data2calc['Game Date_home'].values))):
+for date in list(set(list(data2calc['Game Date_home']))):
 	plac_lin=plac_lin.append(plac.loc[plac['date']==date])
+plac_lin['date']=plac_lin['date'].astype('datetime64[ns]')
 plac_lin=plac_lin.sort_values(['date','home'])
+new=[]
+for x in plac_lin['date']:
+	x=str(x)
+	x=x.replace('-','/')
+	x=x[:10]
+	x=datetime.datetime.strptime(x,"%Y/%m/%d")
+	x=x.strftime("%m/%d/%Y")
+	new.append(x)
+plac_lin['date']=new
 plac_lin.reset_index(inplace=True,drop=True)
 
 # new_lin has the all the games minus the ones predicted and not
 # have been played yet
 new_lin=pd.DataFrame()
-for date in list(set(list(data2calc['Game Date_home'].values))):
+for date in list(set(list(data2calc['Game Date_home']))):
 	new_lin=new_lin.append(lin.loc[lin['date']==date])
+new_lin['date']=new_lin['date'].astype('datetime64[ns]')
 new_lin=new_lin.sort_values(['date','home'])
+new=[]
+for x in new_lin['date']:
+	x=str(x)
+	x=x.replace('-','/')
+	x=x[:10]
+	x=datetime.datetime.strptime(x,"%Y/%m/%d")
+	x=x.strftime("%m/%d/%Y")
+	new.append(x)
+new_lin['date']=new
 new_lin.reset_index(inplace=True,drop=True)
 
 # check if logs are in the same order
@@ -43,18 +76,27 @@ count,spent=0,0
 count_H,spent_H=0,0
 count_A,spent_A=0,0
 count_all=0
+profit,dates=[],[]
 for x in range(len(data2calc['Team_home'])):
 	if float(new_lin.at[x,'linear'][1:-1]) > 0.49:
 		if plac_lin.at[x,'plac_A']>plac_lin.at[x,'plac_H']:
 #			print(new_lin.at[x,'home'],new_lin.at[x,'away'],new_lin.at[x,'date'])
 			if data2calc.at[x,'MIN_home']==48 and data2calc.at[x,'Result']==1:
 #					print('disagreed i said',new_lin.at[x,'away'],data2calc.at[x,'Game Date_home'],plac_lin.at[x,'plac_A'])
-					count+=plac_lin.at[x,'plac_A']
-					count_A+=plac_lin.at[x,'plac_A']
+				count+=plac_lin.at[x,'plac_A']
+				count_A+=plac_lin.at[x,'plac_A']
 			count-=1
 			spent+=1
 			count_A-=1
 			spent_A+=1
+			try:
+				profit.append(count_A/spent_A)
+				#dates.append(plac_lin.at[x,'date'])
+				dates.append(x)
+			except:
+				profit.append(0)
+				dates.append(x)
+				#dates.append(plac_lin.at[x,'date'])
 		if data2calc.at[x,'MIN_home']==48 and data2calc.at[x,'Result']==1:
 			count_all+=plac_lin.at[x,'plac_A']
 
@@ -73,6 +115,9 @@ for x in range(len(data2calc['Team_home'])):
 		if data2calc.at[x,'MIN_home']==48 and data2calc.at[x,'Result']==0:
 			count_all+=plac_lin.at[x,'plac_H']
 
+plt.plot(dates,profit)
+#plt.grid(True)
+plt.show()
 print('\ntotal_H: {:.2f} spent_H: {}'.format(count_H,spent_H))
 print('linear return on disagreed home: {:.4f}%\n'.format(count_H/spent_H *100))
 
@@ -91,19 +136,51 @@ mlp=pd.read_csv('mlp_log.csv')
 data2calc=pd.DataFrame()
 for date in list(set(list(mlp['date'].values))):
 	data2calc=data2calc.append(data.loc[data['Game Date_home']==date])
+data2calc['Game Date_home']=data2calc['Game Date_home'].astype('datetime64[ns]')
 data2calc=data2calc.sort_values(['Game Date_home','Team_home'])
+new=[]
+for x in data2calc['Game Date_home']:
+	x=str(x)
+	x=x.replace('-','/')
+	x=x[:10]
+	x=datetime.datetime.strptime(x,"%Y/%m/%d")
+	x=x.strftime("%m/%d/%Y")
+	new.append(x)
+data2calc['Game Date_home']=new
 data2calc.reset_index(inplace=True,drop=True)
 
+# get plac data on mlp predicted games
 plac_mlp=pd.DataFrame()
 for date in list(set(list(data2calc['Game Date_home'].values))):
 	plac_mlp=plac_mlp.append(plac.loc[plac['date']==date])
+plac_mlp['date']=plac_mlp['date'].astype('datetime64[ns]')
 plac_mlp=plac_mlp.sort_values(['date','home'])
+new=[]
+for x in plac_mlp['date']:
+	x=str(x)
+	x=x.replace('-','/')
+	x=x[:10]
+	x=datetime.datetime.strptime(x,"%Y/%m/%d")
+	x=x.strftime("%m/%d/%Y")
+	new.append(x)
+plac_mlp['date']=new
 plac_mlp.reset_index(inplace=True,drop=True)
 
+# get mlp predicted games
 new_mlp=pd.DataFrame()
 for date in list(set(list(data2calc['Game Date_home'].values))):
 	new_mlp=new_mlp.append(mlp.loc[mlp['date']==date])
+new_mlp['date']=new_mlp['date'].astype('datetime64[ns]')
 new_mlp=new_mlp.sort_values(['date','home'])
+new=[]
+for x in new_mlp['date']:
+	x=str(x)
+	x=x.replace('-','/')
+	x=x[:10]
+	x=datetime.datetime.strptime(x,"%Y/%m/%d")
+	x=x.strftime("%m/%d/%Y")
+	new.append(x)
+new_mlp['date']=new
 new_mlp.reset_index(inplace=True,drop=True)
 
 # check if logs are in the same order
@@ -117,6 +194,7 @@ spent,count=0,0
 spent_A,spent_H=0,0
 count_A,count_H=0,0
 count_all=0
+profit,dates=[],[]
 for x in range(len(data2calc['Team_home'])):
 	if round(float((new_mlp.at[x,'mlp'][1:-1]))) == 1:
 		if plac_mlp.at[x,'plac_A']>plac_mlp.at[x,'plac_H']:
@@ -129,6 +207,14 @@ for x in range(len(data2calc['Team_home'])):
 			spent+=1
 			count_A-=1
 			spent_A+=1
+			try:
+				profit.append(count_A/spent_A)
+				#dates.append(new_mlp.at[x,'date'])
+				dates.append(x)
+			except:
+				profit.append(0)
+				dates.append(x)
+				#dates.append(new_mlp.at[x,'date'])
 	
 		if data2calc.at[x,'MIN_home']==48 and data2calc.at[x,'Result']==1:
 			count_all+=plac_mlp.at[x,'plac_A']
@@ -148,6 +234,10 @@ for x in range(len(data2calc['Team_home'])):
 		if data2calc.at[x,'MIN_home']==48 and data2calc.at[x,'Result']==0:
 			count_all+=plac_mlp.at[x,'plac_H']
 
+plt.plot(dates,profit)
+#plt.ylim(-0.25,1.25)
+#plt.grid(True)
+plt.show()
 print('total_H: {:.2f} spent_H: {}'.format(count_H,spent_H))
 print('perceptron return on disagreed home: {:.4f}%\n'.format(count_H/spent_H *100))
 

7f47026e2e7660cb76bf88b20116fe65856f5f81
space <miranda.junior@live.com.pt>
2020-08-03 14:05:38 +0100

    idk, useless until next season

--- plac_preds.py
+++ plac_preds.py
@@ -18,85 +18,73 @@ from selenium.webdriver.common.by import By
 
 os_name=platform.system()
 
-dates,home_odds,away_odds,homes,aways=[],[],[],[],[]
-def get_stats():
-  if os_name=='Linux':
-    driver = webdriver.Firefox(executable_path='../geckodriver')
-  else:
-    driver = webdriver.Chrome(executable_path='C:/Users/dude/Desktop/chromedriver.exe')
-  driver.get('https://www.jogossantacasa.pt/web/Placard/eventos?id=22911')
-  WebDriverWait(driver,15).until(EC.presence_of_element_located((By.CSS_SELECTOR, "table.wide-table")))
-  
-  html=bs4(driver.page_source,'html.parser')
-  match=re.search('1X2 INT',str(html))
-  """
-  if match:
-    html=str(html)
-    html=html[:match.start()]
-    html=bs4(html,'html.parser')
-  """
-  stats=html.find_all("div", class_="content events")
-  try:
-    stats=bs4((str(stats[0])+str(stats[1])),'html.parser')
-  except:
-    stats=bs4(str(stats[0]),'html.parser')
-  games=stats.find_all("tr")
-      
-  for tr in games:
-    date_div=tr.find_all('td', class_='date')
-    dates.append(re.sub(r'\W+','',date_div[0].text).strip("\n"))
+if os_name=='Linux':
+	driver = webdriver.Firefox(executable_path='../geckodriver')
+else:
+	driver = webdriver.Chrome(executable_path='C:/Users/dude/Desktop/chromedriver.exe')
+driver.get('https://placard.jogossantacasa.pt/PlacardWeb/Events?CompetionName=NBA&RegionName=E.U.A.&SelectedCompetitionId=23164&SelectedDate='+input('year(2020)-month(08)-day(04): ')+'&SelectedModalityId=basketball')
 
-    odds_divs=tr.find_all("div", class_="outcome-wrapper")
-    
-    home=re.sub(r'[^\D]','',odds_divs[0].text)
-    home=re.sub(r'\n','',home)
-    home=re.sub(r'\xa0','',home)
-    home=re.sub(r',','',home)
-    homes.append(home)
+WebDriverWait(driver,15).until(EC.presence_of_element_located((By.ID, "b5-l4-0-b8-ContentWrapper")))
 
-    away=re.sub(r'[^\D]','',odds_divs[2].text)
-    away=re.sub(r'\n','',away)
-    away=re.sub(r'\xa0','',away)
-    away=re.sub(r',','',away)
-    aways.append(away)
+html=bs4(driver.page_source,'html.parser')
 
-    home_odds.append(re.sub(r'[^0-9,]','',odds_divs[0].text).replace(',','.'))
-    away_odds.append(re.sub(r'[^0-9,]','',odds_divs[2].text).replace(',','.'))
+# teams field
+teams=html.find_all("span", class_="font-size20")
 
-  driver.quit()
+# odds field
+odds=html.find_all("span", class_="odd ThemeGrid_MarginGutter")
 
-get_stats()
+driver.quit()
 
-# add the game date
-now = datetime.datetime.now()
-new_dates=[]
-for x in dates:
-  new_dates.append(str(now.month)+'/'+str(now.day)+'/'+str(now.year))
-
+# start preprocessing
 real_games=pd.read_csv('games.csv')
+
+# get only nba teams
+teams=teams[:len(real_games)*2]
+home , away = [],[]
+i=0
+while i < len(teams):
+	if i % 2 == 0:
+		home.append(str(teams[i].text))
+	else:
+		away.append(str(teams[i].text))
+	i=i+1
+
+# get home and away odds
+home_odds , away_odds=[],[]
+odds=odds[-len(home) * 3:]
+i=0
+while i < len(odds):
+	odd=str(odds[i].text).replace(',','.')
+	home_odds.append(float(odd))
+	i=i+3
+i=2
+while i < len(odds):
+	odd=str(odds[i].text).replace(',','.')
+	away_odds.append(float(odd))
+	i=i+3
+
 df=pd.DataFrame()
-df['home']=f.name2acro(homes,'placard')
-df['away']=f.name2acro(aways,'placard')
-df['date']=new_dates
+df['home']=f.name2acro(home,'placard')
+df['away']=f.name2acro(away,'placard')
+df['date']=real_games['date']
 df['plac_H']=home_odds
 df['plac_A']=away_odds
-df=df[:len(real_games)]
+
 df=df.sort_values('home')
+print(df)
 
 if len(real_games)>len(df):
   print('\nmissing {} games on plac_log\n'.format(len(real_games)-len(df)))
   #sys.quit()
 
+"""
 # delete the '76' on philadelphia odds
 for ix in range(len(df)):
   if df.at[ix,'home'] or df.at[ix,'away']=='PHI':
     df.at[ix,'plac_A']=df.at[ix,'plac_A'][-4:]
     df.at[ix,'plac_H']=df.at[ix,'plac_H'][-4:]
-
-#add zeros on date before day or month
-for i in range(len(df)):
-	if int(df.at[i,'date'][:1])<10:
-		df.at[i,'date']='0'+str(df.at[i,'date'])
+"""
 
 curr_path=os.getcwd()
 if os_name=='Linux':
@@ -107,3 +95,4 @@ else:
 log=pd.read_csv(path2logs+'plac_log.csv')
 log=log.append(df,sort=False)
 log.to_csv(path2logs+'plac_log.csv',index=False)
+
